d7b99b1d0205554f5878ecd8b20fec1047f77e15
Jorge Rodriguez <bs.vino@gmail.com>
2004-10-25 14:18:39 +0000

    git-svn-id: http://svn.bsbhd.com/sephiabot/trunk@1 62dfe5c0-45e7-0310-97cb-c0f259deffb2

---
+++ IRCServer.java
@@ -0,0 +1,100 @@
+class IRCServer {
+
+  String network;
+  int port;
+  IRCChannel channels[];
+  IRCUser users[];
+
+  public static int ACCESS_NONE = 0;
+  public static int ACCESS_VOICE = 1;
+  public static int ACCESS_HALFOP = 2;
+  public static int ACCESS_OP = 3;
+
+  IRCServer(String network, int port, String[] channels) {
+    this.network = network;
+    this.port = port;
+    this.channels = new IRCChannel[channels.length];
+    for (int i = 0; i < channels.length; i++) {
+      this.channels[i] = new IRCChannel(channels[i]);
+    }
+  }
+
+}
+
+class IRCChannel {
+
+  String name;
+  IRCUser users;
+  int numusers = 0;
+
+  IRCChannel(String name) {
+    this.name = name;
+  }
+
+  void addUser(String user, int access) {
+
+    if (users == null) {
+      users = new IRCUser(user, access);
+      numusers = 1;
+      return;
+    }
+
+    IRCUser curr = users;
+    while (true) {
+      if (curr.name.equals(user)) {
+        curr.access = access;
+        return;
+      }
+      if (curr.next == null) {
+        curr.next = new IRCUser(user, access);
+        numusers++;
+        return;
+      }
+      curr = curr.next;
+    }
+  }
+
+  void remUser(String user) {
+
+    if (users == null) {
+      numusers = 0;
+      return;
+    }
+
+    if (users.next == null) {
+      users = null;
+      numusers = 0;
+      return;
+    }
+
+    IRCUser curr = users;
+    IRCUser last = users;
+    while (true) {
+      if (curr.name.equals(user)) {
+        last.next = curr.next;
+        numusers--;
+        return;
+      }
+      if (curr.next == null) {
+        return;
+      }
+      last = curr;
+      curr = curr.next;
+    }
+  }
+
+}
+
+class IRCUser {
+
+  String name;
+  int access;
+
+  IRCUser next;
+
+  IRCUser(String name, int access) {
+    this.name = name;
+    this.access = access;
+  }
+
+}
