d5c31784ab88af9331efd914cc34f9bd8ee3b308
Ed Anderson <nilbus@nilbus.com>
2005-10-25 02:56:46 +0000

    add database shcema
    git-svn-id: http://svn.bsbhd.com/sephiabot/trunk@177 62dfe5c0-45e7-0310-97cb-c0f259deffb2

---
+++ dbSchema.sql
@@ -0,0 +1,72 @@
+drop table if not exists config;
+create table config (
+    id int not null auto_increment primary key,
+    customConfigID foreign key default 1;
+    sephiadir tinytext,
+    datafilename tinytext,
+    usersfilename tinytext,
+    logdir tinytext,
+
+drop table if not exists customconfig;
+create table customconfig (
+    id int not null auto_increment primary key,
+    hello tinytext;
+    censor boolean;
+    greeting tinytext;
+    helloreplies tinytext;
+    
+drop table if not exists server;
+create table server (
+    id int not null auto_increment primary key,
+    host tinytext not null,
+    port int,
+    nick tinytext not null);
+    
+drop table if not exists channel;
+create table channel (
+    id int not null auto_increment primary key,
+    serverID int not null foreign key,
+    name tinytext not null,
+    greeting tinytext);
+
+drop table if not exists blacklist;
+create table blacklist (
+    id int not null auto_increment primary key,
+    regex tinytext not null);
+
+drop table if not exists group;
+create table group (
+    id int not null auto_increment primary key,
+    name tinytext not null);
+
+drop table if not exists user;
+create table groupPermission (
+    id int not null auto_increment primary key,
+    groupID int not null foreign key,
+    varchar(32) name not null);
+        
+drop table if not exists host;
+create table host (
+    id int not null auto_increment primary key,
+    userID int not null foreign key,
+    hostname tinytext not null);
+
+drop table if not exists message;
+create table message (
+    id int not null auto_increment primary key,
+    target tinytext not null,
+    sender tinytext not null,
+    message tinytext not null,
+    timeSent long not null,
+    timeToArrive long not null,
+    notified boolean default 0);
+
+drop table if not exists paste;
+create table paste (
+    id int not null auto_increment primary key,
+    channelID int foreign key,
+    nick tinytext,
+    summary tinytext,
+    contents mediumtext,
+    submittime datetime,
+    IP tinytext);
