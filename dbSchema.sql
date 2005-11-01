a74ec9c764da79cc6dd56a8f37667cee0e4c1ad8
Ed Anderson <nilbus@nilbus.com>
2005-11-01 01:59:59 +0000

    more schema changes
    git-svn-id: http://svn.bsbhd.com/sephiabot/trunk@180 62dfe5c0-45e7-0310-97cb-c0f259deffb2

--- dbSchema.sql
+++ dbSchema.sql
@@ -1,4 +1,4 @@
-drop table if not exists config;
+drop table if exists config;
 create table config (
     id int not null auto_increment primary key,
     customConfigID default 1;
@@ -7,52 +7,74 @@ create table config (
     usersfilename tinytext,
     logdir tinytext,
 
-drop table if not exists customconfig;
-create table customconfig (
+drop table if exists customConfig;
+create table customConfig (
     id int not null auto_increment primary key,
     hello tinytext;
     censor boolean;
     greeting tinytext;
     helloreplies tinytext;
     
-drop table if not exists server;
+drop table if exists server;
 create table server (
     id int not null auto_increment primary key,
     host tinytext not null,
     port int,
-    nick tinytext not null);
+    nick tinytext not null,
+    customConfigID int);
     
-drop table if not exists channel;
+drop table if exists channel;
 create table channel (
     id int not null auto_increment primary key,
     serverID int not null,
     name tinytext not null,
-    greeting tinytext);
+    greeting tinytext,
+    customConfigID int);
 
-drop table if not exists blacklist;
+drop table if exists blacklist;
 create table blacklist (
     id int not null auto_increment primary key,
-	customConfigID int not null,
-    regex tinytext not null);
+    channelID int,
+    serverID int,
+    nickRegex tinytext not null);
 
-drop table if not exists group;
+drop table if exists group;
 create table group (
     id int not null auto_increment primary key,
     name tinytext not null);
 
-drop table if not exists user;
+drop table if exists groupPermission;
 create table groupPermission (
     id int not null auto_increment primary key,
     groupID int not null,
     varchar(32) name not null);
+
+drop table if exists groupMembership;
+create table groupMembership (
+    id int not null auto_increment primary key,
+    groupID int not null,
+    userID int not null);
+
+drop table if exists user;
+create table user (
+    id int not null auto_increment primary key,
+    nick tinytext not null,
+    password tinytext,
+    description tinytext);
+    
+drop table if exists alias;
+create table alias (
+    id int not null auto_increment primary key,
+    alias tinytext not null,
+    userID int not null);
         
-drop table if not exists host;
+drop table if exists host;
 create table host (
     id int not null auto_increment primary key,
     userID int not null,
     hostname tinytext not null);
 
-drop table if not exists message;
+drop table if exists message;
 create table message (
     id int not null auto_increment primary key,
     target tinytext not null,
@@ -62,7 +84,7 @@ create table message (
     timeToArrive long not null,
     notified boolean default 0);
 
-drop table if not exists paste;
+drop table if exists paste;
 create table paste (
     id int not null auto_increment primary key,
     channelID int,
@@ -72,7 +94,7 @@ create table paste (
     submittime datetime,
     IP tinytext);
 
-drop table if not exists alert;
+drop table if exists alert;
 create table alert (
     id int not null auto_increment primary key,
     channelID int,
